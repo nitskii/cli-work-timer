64c4ec4098c5bf2639ffdc32b9cfb238b730f9d9
Nikita Miller <nikita.omelnitskii@gmail.com>
2023-11-22 12:26:29 +0200

    added durations to timecard info

--- src/lib.js
+++ src/lib.js
@@ -11,13 +11,12 @@ const getCurrentTime = () => {
 };
 
 export const timeArgument = new Argument(
-    '[time]',
-    'time to set, e.g. 19:31, 5:44 PM'
-  )
-  .default(
-    getCurrentTime(),
-    'current time'
-  );
+  '[time]',
+  'time to set, e.g. 19:31, 5:44 PM'
+).default(
+  getCurrentTime(),
+  'current time'
+);
 
 const isValidTime = time =>
   moment(time, 'H:mm', true).isValid() ||
@@ -79,24 +78,51 @@ export const displayCheckedOutMessage = ({ processedArgs }) => {
   console.log(`Checked out at ${time}`);
 };
 
+const getTimeRangeDuration = (start, end) => {
+  const [ startHours, startMinutes ]= start.split(':');
+  const startSeconds = startHours * 3600 + startMinutes * 60;
+
+  const [ endHours, endMinutes ]= end.split(':');
+  const endSeconds = endHours * 3600 + endMinutes * 60;
+
+  const duration = Math.abs(endSeconds - startSeconds);
+  const hours = Math.floor(duration / 3600);
+  const minutes = duration % 3600 / 60;
+
+  return `${hours ? `${hours}h ` : ""}${minutes}m`;
+};
+
 export const displayCurrentTimecard = () => {
   const today = new Date().toLocaleDateString('uk-UA');
-  const timecard = timecards[today];
+  const records = timecards[today];
   
   console.log(`${today}:`);
 
-  if (!timecard) {
-    console.log("No records today yet");
+  if (!records) {
+    console.log('No records today yet');
 
     return;
   }
 
-  timecard.forEach(
+  records.forEach(
     (r, i) => console.log(
       `${i + 1}. ` +
-      `${r.start || 'N/A'}` +
+      `${r.start && r.start.length == 4 ? ' ' : ''}` +
+      `${r.start || '  N/A'}` +
       ' - ' +
-      `${r.end || 'N/A'}`
+      `${r.end && r.end.length == 4 ? ' ' : ''}` +
+      `${r.end || '  N/A'}` + 
+      (r.start && r.end
+        ? `, ${getTimeRangeDuration(r.start, r.end)}`
+        : ''
+      )
     )
   );
+
+  if (records.some(r => r.start && r.end)) {
+    console.log(`Total: ${getTimeRangeDuration(
+      records[0].start,
+      records.findLast(r => r.end).end
+    )}`);
+  }
 };
