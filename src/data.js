6a46e0bcc46f03233a034a25e7e2be4c0a7c0538
Nikita Miller <nikita.omelnitskii@gmail.com>
2023-11-22 11:11:43 +0200

    added data formatting on data save

--- src/data.js
+++ src/data.js
@@ -4,7 +4,7 @@ import { writeFile } from 'fs/promises';
 const DATA_FILE_NAME = 'timecards.json';
 
 export const saveData = async () => {
-  await writeFile(DATA_FILE_NAME, `${JSON.stringify(data)}\n`);
+  await writeFile(DATA_FILE_NAME, `${JSON.stringify(data, null, 2)}\n`);
 };
 
 export const data = existsSync(DATA_FILE_NAME)
