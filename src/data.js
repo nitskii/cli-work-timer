8811c45d99b62a58e9bf150817ca32b855597fe9
Nikita Miller <nikita.omelnitskii@gmail.com>
2023-12-11 11:47:19 +0200

    updated data file location to be consistent

--- src/data.js
+++ src/data.js
@@ -1,7 +1,18 @@
-import { existsSync, readFileSync } from 'fs';
+import {
+  existsSync,
+  mkdirSync,
+  readFileSync
+} from 'fs';
 import { writeFile } from 'fs/promises';
+import { homedir, platform } from 'os';
 
-const DATA_FILE_NAME = 'timecards.json';
+const APP_FOLDER = platform() == 'win32'
+  ? `${process.env.LOCALAPPDATA}/work-timer`
+  : `${homedir()}/.work-timer`;
+
+existsSync(APP_FOLDER) || mkdirSync(APP_FOLDER);
+
+const DATA_FILE_NAME = `${APP_FOLDER}/timecards.json`;
 
 export const saveData = async () => {
   await writeFile(DATA_FILE_NAME, `${JSON.stringify(data, null, 2)}\n`);
